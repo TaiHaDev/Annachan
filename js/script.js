61f86abae2fb4cdfa71ef51e4c431e754ffccb8e
Terris711 <terristerris200501@gmail.com>
2023-07-29 23:21:19 +0800

    Data for Filter

---
+++ js/script.js
@@ -0,0 +1,114 @@
+// Dummy data for fauna
+const Fauna = [
+    {
+        species: {
+            name: 'fauna3',
+            imageURL: './image/Forest/fauna3.png',
+            Morophological: ['Beak', '6 legs', 'antlers', '2 eyes', 'orange skin', 'No tail'],
+            Genetic_Structure: 'DNA structure (Z)  - there is no form of species that exist on earth that have 6 limbs. Hence, we have to generate a new form of DNA structure',
+            Behaviour: ['when they are in danger. They send signals by shouting to their friend',
+                        'Large eye for night vision and 360 views',
+                        'Use peak to attack and penetrate hard surface for food'],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna4',
+            imageURL: './image/Forest/fauna4.png',
+            Morophological: ['Exoskeleton', '4 legs', '2 arms', '2 eyes', 'No Tail', 'trunk mouth'],
+            Genetic_Structure: 'DNA structure (G+C) - This DNA structure is similar to some of the insects found on earth which is why we don’t have to develop a new category/DNA structure from the insect. DNA structures can be similar but morphological (physical features) can vary',
+            Behaviour: ['when in danger they transform themselves into a ball and roll',
+                        'when they consume food, they spin the prey around 3-5 circles',
+                        'Never alone always keep in a group of 10- 20'
+            ],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna5',
+            imageURL: './image/Forest/fauna5.png',
+            Morophological: ['4 legs', '2 eyes', 'smooth reptilian skin', '1 tail', 'mouth'],
+            Genetic_Structure: 'The DNA structure is a mixture of a dinosaur and a lizard',
+            Behaviour: ['When is danger they eject venomous liquid from the hanging tail',
+                        'The pointy tip of the tail can regrow once in a month time',
+                        'The point tip of the tail can spike into the prey and eject venom to paralyze their'
+            ],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna7',
+            imageURL: './image/Forest/fauna7.png',
+            Morophological: ['2 eyes', '2 legs', 'smooth skin', 'No tail', 'mouth'],
+            Genetic_Structure:  'A combination of a rhino and frog because the creature is similar to a frog but has the facial features of a rhino',
+            Behaviour: ['when in danger the little spike can automatically stretch out',
+                        'Live under the water for more than 3 hour',
+                        'Thick skin layer that spike can not penetrate through'],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna9',
+            imageURL: './image/Forest/fauna9.png',
+            Morophological: ['2 eyes', 'scaly skin', '4 legs', 'mouth'],
+            Genetic_Structure: 'Type Z due to having a multicoloured skin while also having features to camouflage to its surrounding environment',
+            Behaviour: ['They can blend their skin color into the surrounding',
+                        'Their eyes can detect through the water',
+                        'They can burst flames'],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna14',
+            imageURL: './image/Forest/fauna14.png',
+            Morophological: ['Feathers', 'beak', '2 eyes', '2 wings', '2 legs', 'gills'],
+            Genetic_Structure: 'Amphibian can survive both on land and water due to having wings to fly and gills to breathe under water',
+            Behaviour: ['Their yellow hair only grows when they have their birthday',
+                        'Feathers are waterproof',
+                        'Only hunt during night time, their red eyes provides infrared radiation'],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna17',
+            imageURL: './image/Forest/fauna17.png',
+            Morophological: ['2 hands', '4 legs', '2 eyes', 'mouth', 'exoskeleton'],
+            Genetic_Structure: 'The exoskeleton makes the insect similar to a grasshopper and cockroach',
+            Behaviour: ['parasites',
+                        'Only feeds on dead aliens']
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna21',
+            imageURL: './image/Forest/fauna21.png',
+            Morophological: ['Scaly skin', 'beak', '2 eyes', '4 legs'],
+            Genetic_Structure: 'Similar to a porcupine because of the scaly features',
+            Behaviour: ['Lives on dry habitat',
+                        'When in danger they turn themselves into a ball similar to porcupine'
+            ],
+        },
+    },
+
+    {
+        species: {
+            name: 'fauna1',
+            imageURL: './image/Forest/fauna21.png',
+            Morophological: ['Scaly skin', 'beak', '2 eyes', '4 legs'],
+            Genetic_Structure: 'Similar to a porcupine because of the scaly features',
+            Behaviour: ['Lives on dry habitat',
+                'When in danger they turn themselves into a ball similar to porcupine'
+            ],
+        },
+    },
+];
+
+
+
