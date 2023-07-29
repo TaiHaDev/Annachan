a5bc27c676f4b2988d8ebe54450d948b90b78db6
Tai Ha <u7454578@anu.edu.au>
2023-07-30 03:22:14 +1000

    new data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { useRef, useState } from "react";
-import bg from "./asset/bg.jpeg";
+import bg from "./image/bg.jpeg";
 import { data } from "./Dummy";
-import logo from "./asset/logo.jpeg"
-import React from 'react'
+import logo from "./image/logo.jpeg";
+import React from "react";
 import Filter from "./Filter";
 import { getRandomLightColor } from "./ultilities/GenerateColor";
 
-
-const transformData = data => {
+const transformData = (data) => {
   const filterData = {};
-  data.forEach(e => {
-    e.attributes.forEach(fil => {
+  data.forEach((e) => {
+    e.Morophological.forEach((fil) => {
       if (filterData[fil]) {
         filterData[fil].push(e);
       } else {
@@ -18,11 +17,10 @@ const transformData = data => {
       }
     });
   });
-  return filterData; 
+  return filterData;
 };
-  
+
 function App() {
-  console.log(getRandomLightColor())
   const [displayingData, setDisplayingData] = useState(data);
   const [filterData, setFilterData] = useState(transformData(data));
   const [chosenFilter, setChosenFilter] = useState([]);
@@ -32,59 +30,89 @@ function App() {
     if (query === "") {
       setDisplayingData(data);
     } else {
-      setDisplayingData(data.filter(obj => obj.attributes.includes(query.toLowerCase())));
+      setDisplayingData(
+        data.filter((obj) => {
+          const res = obj.Morophological.filter(str => str.includes(query.toLowerCase()));
+          return res.length > 0;
+        })
+      );
     }
-  }
+  };
   return (
-    <div style={{ 
-      position: "relative", 
-      backgroundImage: `url(${bg})`,
-      backgroundRepeat: "repeat",
-      backgroundSize: "cover",
-      width: '100vw',
-      height: '100vh',
-      overflow: 'auto'
-    }}
-    className="text-xs md:text-xl text-white"
+    <div
+      style={{
+        position: "relative",
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: "repeat",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+        overflow: "auto",
+      }}
+      className="text-xs md:text-xl text-white"
     >
       <div>
-        <img src={logo} alt=""  className="invisible md:visible absolute top-8 left-8 w-16 h-16 rounded-md"/>
+        <img
+          src={logo}
+          alt=""
+          className="invisible md:visible absolute top-8 left-8 w-16 h-16 rounded-md"
+        />
         <h1 class="mt-12 text-xl md:text-4xl text-center font-medium text-transparent bg-clip-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-amber-700 via-orange-300 to-rose-800">
-        ALIEN ENCYCLOPEDIA BY <span className="text-lg bg-clip-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-300 via-fuchsia-600 to-orange-600">Hackerstreet Boys</span>
-</h1>
+          ALIEN ENCYCLOPEDIA BY{" "}
+          <span className="text-lg bg-clip-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-300 via-fuchsia-600 to-orange-600">
+            Hackerstreet Boys
+          </span>
+        </h1>
       </div>
 
-      <div
-        className="m-4 md:m-8 lg:m-12 text-center"
-      >
+      <div className="m-4 md:m-8 lg:m-12 text-center">
         <div className="flex items-center justify-center">
-        <input
-          ref={searchRef}
-          type="text"
-          className="border-b border-white w-[20rem] md:w-[40rem] outline-none bg-transparent  font-medium placeholder:font-medium"
-          placeholder="Search for your favourite space creatures"
-          onChange={onInputChange}
-        />
-        <Filter filters={filterData} setDisplayingData={setDisplayingData} chosenFilter={chosenFilter} setChosenFilter={setChosenFilter} />
+          <input
+            ref={searchRef}
+            type="text"
+            className="border-b border-white w-[20rem] md:w-[40rem] outline-none bg-transparent  font-medium placeholder:font-medium"
+            placeholder="Search for your favourite space creatures"
+            onChange={onInputChange}
+          />
+          <Filter
+            filters={filterData}
+            setDisplayingData={setDisplayingData}
+            chosenFilter={chosenFilter}
+            setChosenFilter={setChosenFilter}
+          />
         </div>
 
-        <div
-          className="flex flex-wrap items-center justify-center mt-8 overflow-hidden gap-2 md:gap-8 "
-          
-        >
+        <div className="flex flex-wrap items-center justify-center mt-8 overflow-hidden gap-2 md:gap-8 ">
           {displayingData.map((obj) => (
-            <div className="flip-card w-32 h-32 md:w-40 md:h-40 lg:w-96 lg:h-96">
+            <div className="flip-card w-32 h-32 md:w-40 md:h-40 lg:w-96 lg:h-96 rounded-md">
               <div className="flip-card-inner">
                 <div className="flip-card-front ">
                   <img
-                    src={obj.src}
+                    src={obj.imageURL}
                     alt=""
-                    className="object-cover w-32 h-32 md:w-40 md:h-40 lg:w-96 lg:h-96 rounded-md"
+                    className="object-cover w-32 h-32 md:w-40 md:h-40 lg:w-96 lg:h-96 rounded-lg"
                   />
                 </div>
-                <div className={`flip-card-back bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 flex flex-col items-center justify-center p-2`} >
-                  <p className="text-slate-800 font-medium text-xl">Annachan</p>
-                  <p className="text-sm text-center">Unique, rare, pretty and not FAT ❤️</p>
+                <div
+                  className={`flip-card-back bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 flex flex-col items-center justify-center p-2`}
+                >
+                  <div className="">
+                    <p >Name:</p>
+                    <p className="text-black text-sm">{obj.name}</p>
+                  </div>
+                  <div>
+                    <p >Genetic Structure:</p>
+                    <p className="text-sm text-black">
+                      {obj.Genetic_Structure}
+                    </p>
+                  </div>
+                  <div>
+                    <p >Behaviour: </p>
+                  <p className="text-sm text-center text-black">
+                    ${obj.Behaviour}
+                  </p>
+                  </div>
+
                 </div>
               </div>
             </div>
--- src/Dummy.js
+++ src/Dummy.js
@@ -1,63 +1,83 @@
-import anna from "./asset/annachan.jpeg"
-export const data = [
-    {
-        src: anna,
-        attributes: ["tai"]
-    },
-    {
-        src: anna,
-        attributes: ["tai"]
-    },
-    {
-        src: anna,
-        attributes: ["tai"]
-    },
-    {
-        src: anna,
-        attributes: ["tai"]
-    },
-    {
-        src: anna,
-        attributes: ["tai"]
-    },
-    {
-        src: anna,
-        attributes: ["vanh"]
-    },
-    {
-        src: anna,
-        attributes: ["anna"]
-    },
-    {
-        src: anna,
-        attributes: ["terris"]
-    },
-    {
-        src: anna,
-        attributes: ["john"]
-    },
-    {
-        src: anna,
-        attributes: []
-    },    {
-        src: anna,
-        attributes: []
-    },
-    {
-        src: anna,
-        attributes: []
-    },
-    {
-        src: anna,
-        attributes: []
-    },
-    {
-        src: anna,
-        attributes: []
-    },
-    {
-        src: anna,
-        attributes: []
-    },
+import fauna3 from "./image/Forest/fauna3.png";
+import fauna4 from "./image/Forest/fauna4.png";
+import fauna5 from "./image/Forest/fauna5.png";
+import fauna7 from "./image/Forest/fauna7.png";
+import fauna9 from "./image/Forest/fauna9.png";
+import fauna14 from "./image/Forest/fauna14.png";
+import fauna17 from "./image/Forest/fauna17.png";
+import fauna21 from "./image/Forest/fauna21.png";
+import fauna1 from "./image/Mountain/fauna1.png";  
 
-]
+export const data = [
+  {
+    name: "fauna3",
+    imageURL: fauna3,
+    Morophological: ["Beak", "6 legs", "antlers", "2 eyes", "orange skin", "No tail"],
+    Genetic_Structure: "DNA structure (Z)  - there is no form of species that exist on earth that have 6 limbs. Hence, we have to generate a new form of DNA structure",
+    Behaviour: ["when they are in danger. They send signals by shouting to their friend", "Large eye for night vision and 360 views", "Use peak to attack and penetrate hard surface for food"],
+  },
+
+  {
+    name: "fauna4",
+    imageURL: fauna4,
+    Morophological: ["Exoskeleton", "4 legs", "2 arms", "2 eyes", "No Tail", "trunk mouth"],
+    Genetic_Structure: "DNA structure (G+C) - This DNA structure is similar to some of the insects found on earth which is why we don’t have to develop a new category/DNA structure from the insect. DNA structures can be similar but morphological (physical features) can vary",
+    Behaviour: ["when in danger they transform themselves into a ball and roll", "when they consume food, they spin the prey around 3-5 circles", "Never alone always keep in a group of 10- 20"],
+  },
+
+  {
+    name: "fauna5",
+    imageURL: fauna5,
+    Morophological: ["4 legs", "2 eyes", "smooth reptilian skin", "1 tail", "mouth"],
+    Genetic_Structure: "The DNA structure is a mixture of a dinosaur and a lizard",
+    Behaviour: ["When is danger they eject venomous liquid from the hanging tail", "The pointy tip of the tail can regrow once in a month time", "The point tip of the tail can spike into the prey and eject venom to paralyze their"],
+  },
+
+  {
+    name: "fauna7",
+    imageURL: fauna7,
+    Morophological: ["2 eyes", "2 legs", "smooth skin", "No tail", "mouth"],
+    Genetic_Structure: "A combination of a rhino and frog because the creature is similar to a frog but has the facial features of a rhino",
+    Behaviour: ["when in danger the little spike can automatically stretch out", "Live under the water for more than 3 hour", "Thick skin layer that spike can not penetrate through"],
+  },
+
+  {
+    name: "fauna9",
+    imageURL: fauna9,
+    Morophological: ["2 eyes", "scaly skin", "4 legs", "mouth"],
+    Genetic_Structure: "Type Z due to having a multicoloured skin while also having features to camouflage to its surrounding environment",
+    Behaviour: ["They can blend their skin color into the surrounding", "Their eyes can detect through the water", "They can burst flames"],
+  },
+
+  {
+    name: "fauna14",
+    imageURL: fauna14,
+    Morophological: ["Feathers", "beak", "2 eyes", "2 wings", "2 legs", "gills"],
+    Genetic_Structure: "Amphibian can survive both on land and water due to having wings to fly and gills to breathe under water",
+    Behaviour: ["Their yellow hair only grows when they have their birthday", "Feathers are waterproof", "Only hunt during night time, their red eyes provides infrared radiation"],
+  },
+
+  {
+    name: "fauna17",
+    imageURL: fauna17,
+    Morophological: ["2 hands", "4 legs", "2 eyes", "mouth", "exoskeleton"],
+    Genetic_Structure: "The exoskeleton makes the insect similar to a grasshopper and cockroach",
+    Behaviour: ["parasites", "Only feeds on dead aliens"],
+  },
+
+  {
+    name: "fauna21",
+    imageURL: fauna21,
+    Morophological: ["Scaly skin", "beak", "2 eyes", "4 legs"],
+    Genetic_Structure: "Similar to a porcupine because of the scaly features",
+    Behaviour: ["Lives on dry habitat", "When in danger they turn themselves into a ball similar to porcupine"],
+  },
+
+  {
+    name: "fauna1",
+    imageURL: fauna1,  //Check this imageURL, it seems to be the same as fauna21
+    Morophological: ["Scaly skin", "beak", "2 eyes", "4 legs"],
+    Genetic_Structure: "Similar to a porcupine because of the scaly features",
+    Behaviour: ["Lives on dry habitat", "When in danger they turn themselves into a ball similar to porcupine"],
+  },
+];
